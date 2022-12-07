c20e31c774c104fd73b80d358125e8b535ad7dec
Chris Waters <cp-waters@hotmail.co.uk>
2022-12-07 17:04:18 +0000

    updates

--- api-frontend/src/components/stock/index.jsx
+++ api-frontend/src/components/stock/index.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import useFetch from '../../hooks/useFetch';
 
 const Users = () => {
-  const { data, loading } = useFetch(`http://localhost:5555/api/stock`);
-  console.log(data);
+    const { data, loading } = useFetch(`http://localhost:5555/api/stock`);
+    console.log(data);
 
-  return (
-    loading ? <div className="App-logo"></div> :
-    <>
-      <h1>Stock</h1>
-      {data && data.map((item, i) => <h4 key={i}>{item.make}</h4>)}
-    </>
-  );
+    return (
+        loading ? <div className="App-logo"></div> :
+        <>
+            <h1>Stock</h1>
+            {data && data.map((item, i) => <h4 key={i}>{item.make}</h4>)}
+        </>
+    );
 };
 
 export default Users;
